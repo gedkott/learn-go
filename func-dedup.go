9a7883784fa9215c38078c5a09ab8a9745ebff04
Gedalia Kott <gedkott@gmail.com>
2018-05-06 19:01:39 -0700

    Uses Cond for each result key

--- func-dedup.go
+++ func-dedup.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"math/rand"
-	"os"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -16,71 +13,96 @@ var fs int32
 type result struct {
 	val       string
 	expiresAt time.Time
+	c         *sync.Cond
+	isRunning bool
 }
 
 type dedup struct {
-	mu      sync.Mutex
-	limit   time.Duration
-	f       func(string) string
-	results map[string]*result
+	resultsMu *sync.Mutex
+	limit     time.Duration
+	f         func(string) string
+	results   map[string]*result
 }
 
 func newDedup(limit time.Duration, f func(string) string) *dedup {
 	return &dedup{
-		limit:   limit,
-		f:       f,
-		results: make(map[string]*result),
+		resultsMu: new(sync.Mutex),
+		limit:     limit,
+		f:         f,
+		results:   make(map[string]*result),
 	}
 }
 
 func (d *dedup) run(x string) string {
-	d.mu.Lock()
-	// Playing around with waiting on go routines
-	defer wg.Done()
-	defer d.mu.Unlock()
-
+	d.resultsMu.Lock()
 	r, ok := d.results[x]
 	if !ok {
-		r = &result{}
+		mu := new(sync.Mutex)
+		c := sync.NewCond(mu)
+		r = &result{c: c, isRunning: false}
 		d.results[x] = r
 	}
-	if time.Now().After(r.expiresAt) {
-		r.val = d.f(x)
-		r.expiresAt = time.Now().Add(d.limit)
+	d.resultsMu.Unlock()
+
+	if !time.Now().After(r.expiresAt) {
+		return r.val
 	}
+
+	r.c.L.Lock()
+
+	for r.isRunning {
+		r.c.Wait()
+	}
+
+	if !time.Now().After(r.expiresAt) {
+		r.c.L.Unlock()
+		r.c.Broadcast()
+		return r.val
+	}
+
+	r.isRunning = true
+	r.val = d.f(x)
+	r.expiresAt = time.Now().Add(d.limit)
+	r.isRunning = false
+	r.c.L.Unlock()
+	r.c.Broadcast()
+
 	return r.val
 }
 
 func main() {
-	logger := log.New(os.Stdout, "func-dedup", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile)
+	// logger := log.New(os.Stdout, "func-dedup", log.Ldate|log.Ltime|log.Lmicroseconds|log.Llongfile)
 
 	d := newDedup(time.Millisecond*100, func(s string) string {
 		atomic.AddInt32(&fs, 1)
 
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		n := r.Int31n(10)
+		// r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		// n := r.Int31n(10)
 
-		logger.Printf("Executing F, calling d.run; first sleeping for %d seconds\n", n)
-		time.Sleep(time.Duration(n) * time.Second)
+		// logger.Printf("Executing F, calling d.run; first sleeping for %d seconds\n", n)
+		// time.Sleep(time.Duration(n) * time.Second)
 
 		return s
 	})
 
 	// Multiple competing goroutines so that there is lock contention
-	for i := 0; i < 100; i = i + 1 {
-		time.Sleep(100 * time.Millisecond)
+	for i := 0; i < 1000; i = i + 1 {
+		if i%100 == 0 {
+			time.Sleep(100 * time.Millisecond)
+		}
 		input := "G"
 		wg.Add(1)
-		logger.Println("generating goroutine", i)
+		// logger.Println("generating goroutine", i)
 		go func(s string, id int) {
 			d.run(s)
-			logger.Println(id, "done executing dedup")
+			// logger.Println(id, "done executing dedup")
+			defer wg.Done()
 		}(input, i)
 	}
 
 	wg.Wait()
 
-	logger.Println("All done")
+	// logger.Println("A	ll done")
 
 	ffs := atomic.LoadInt32(&fs)
 
